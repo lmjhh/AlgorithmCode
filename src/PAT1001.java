f07af08d3d521ae1f7724caa7907c4f47d7e9b66
Hrh <576788582@qq.com>
2016-02-25 11:38:38 +0800

    一题

---
+++ src/PAT1001.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+// PAT 1001	A+B和C 
+public class PAT1001 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t;
+		long  a,b,c;
+		Scanner scanner = new Scanner(System.in);
+		t = scanner.nextInt();
+		for(int i=1;i<=t;i++){
+			
+			a = scanner.nextLong();
+			b = scanner.nextLong();
+			c = scanner.nextLong();
+			
+		     if( a+b > c){
+		    	 System.out.printf("Case #%d: true\n", i);
+		     }
+		     else System.out.printf("Case #%d: false\n", i);
+		}
+	}
+}
